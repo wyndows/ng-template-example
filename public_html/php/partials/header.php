08cfdcc5b170fbd5d88102cf61195f9c0011c140
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-03-10 13:47:29 -0700

    insert header.php

---
+++ public_html/php/partials/header.php
@@ -0,0 +1,24 @@
+<header>
+	<div class="container">
+		<nav class="navbar navbar-inverse">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="/~rlewis37/ng-template-example/public_html/">MeowApp</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="/~rlewis37/ng-template-example/public_html/about">About</a></li>
+					<li><a href="/~rlewis37/ng-template-example/public_html/toys">Toys</a></li>
+					<li><a href="/~rlewis37/ng-template-example/public_html/treats">Kitty Treats</a></li>
+					<li><a href="https://senator-arlo.bowtied.io/" target="_blank">Feel the Fuzzy</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</nav>
+	</div><!--/.container-->
+</header>
