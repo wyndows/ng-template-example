fb6297b52025946c4d3132fc1484794c4a4b8223
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-03-10 11:06:48 -0700

    add starter files

---
+++ public_html/toys.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="NgMeow">
+	<head>
+		<!-- The 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- set base for relative links - to enable pretty URLs. -->
+		<base href="/~rlewis37/angular-template/public_html/">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- My Custom CSS rlewis37@cnm.edu -->
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+		<!--Angular JS-->
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-rc.0/angular.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.js"></script>
+
+		<!--Angular application files-->
+		<script type="text/javascript" src="angular/app.js"></script>
+
+		<title>Meow Template!</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<header>
+				<div class="container">
+					<nav class="navbar navbar-inverse">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="/~rlewis37/ng-template-example/public_html/">MeowApp</a>
+						</div>
+
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="/~rlewis37/ng-template-example/public_html/about">About</a></li>
+								<li><a href="/~rlewis37/ng-template-example/public_html/toys">Toys</a></li>
+								<li><a href="/~rlewis37/ng-template-example/public_html/treats">Kitty Treats</a></li>
+								<li><a href="https://senator-arlo.bowtied.io/" target="_blank">Feel the Fuzzy</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</nav>
+				</div><!--/.container-->
+			</header>
+
+			<main class="p-y-4">
+				<div class="container">
+
+					<div class="col-xs-12">
+						Toys
+					</div>
+
+				</div>
+			</main>
+
+		</div><!--/.sfooter-content-->
+		<footer class="p-y-4">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 m-b-2">
+						<strong>=^. .^=</strong>
+					</div>
+					<div class="col-md-6 m-b-2 text-right-md-up">
+						&copy;&nbsp;2016 Romulan Star Empire
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
